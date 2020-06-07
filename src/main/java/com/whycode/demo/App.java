544f097944aa0ccaddbc8f82b8e890c802acf6de
edison chen <edisonchen2019@gmail.com>
2020-06-07 10:37:30 -0400

    my first maven commit

---
+++ src/main/java/com/whycode/demo/App.java
@@ -0,0 +1,13 @@
+package com.whycode.demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
